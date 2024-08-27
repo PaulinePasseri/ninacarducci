93a943390d53bba1c495c727b9d696f09165e03f
Pauline Passeri <paulinecpasseri@gmail.com>
2024-08-27 18:43:16 +0200

    création d'un fichier Json et galerie créée en JS

--- assets/scripts.js
+++ assets/scripts.js
@@ -1,15 +1,39 @@
-$(document).ready(function() {
-    $('.gallery').mauGallery({
-        columns: {
-            xs: 1,
-            sm: 2,
-            md: 3,
-            lg: 3,
-            xl: 3
-        },
-        lightBox: true,
-        lightboxId: 'myAwesomeLightbox',
-        showTags: true,
-        tagsPosition: 'top'
-    });
-});
+// $(document).ready(function() {
+//     $('.gallery').mauGallery({
+//         columns: {
+//             xs: 1,
+//             sm: 2,
+//             md: 3,
+//             lg: 3,
+//             xl: 3
+//         },
+//         lightBox: true,
+//         lightboxId: 'myAwesomeLightbox',
+//         showTags: true,
+//         tagsPosition: 'top'
+//     });
+// });
+
+
+let gallery = document.querySelector(".gallery")
+let works = []
+
+function getWorks() {
+    fetch("../images.json").then(response => {
+        response.json().then(data => {
+            works = data
+            generateGallery(works)
+        })
+    })
+}
+
+// Galerie générée 
+function generateGallery(images) {
+    gallery.innerHTML = ""
+    images.forEach(image => {
+        gallery.innerHTML += 
+        `<img src="${image.src}" alt="${image.alt}" id="${image.id}">`
+    })
+}
+
+getWorks()
